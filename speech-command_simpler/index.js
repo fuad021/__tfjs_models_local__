266bed5e3da8a30d201dbc096061277da9b07c5e
FuadAlAbir <amusafeer@gmail.com>
2020-07-13 07:03:33 +0600

    5 :: tfjs-speech-command

--- speech-command_simpler/index.js
+++ speech-command_simpler/index.js
@@ -1,21 +1,49 @@
 let recognizer;
 
 function predictWord() {
- // Array of words that the recognizer is trained to recognize.
- const words = recognizer.wordLabels();
- recognizer.listen(({scores}) => {
-   // Turn scores into a list of (score,word) pairs.
-   scores = Array.from(scores).map((s, i) => ({score: s, word: words[i]}));
-   // Find the most probable word.
-   scores.sort((s1, s2) => s2.score - s1.score);
-   document.querySelector('#whiteboard').textContent = scores[0].word;
- }, {probabilityThreshold: 0.75});
+    const words = recognizer.wordLabels();
+    const threshold = 0.9;
+    
+    /* particular wordlist variables
+    var global_recognizer = '';
+    var score = -1;
+    var yes_score = -1;
+    var go_score = -1;
+    var time = Date.now();
+    var time_th = 1500;
+    */
+    
+    recognizer.listen(({scores}) => {
+        scores = Array.from(scores).map((s, i) => ({score: s, word: words[i]}));
+        
+        // for particular wordlist :: ['yes', 'go']
+        /*
+        yes_score = scores[18].score;
+        go_score = scores[6].score;
+        
+        if (go_score > yes_score && go_score > threshold && time + time_th < Date.now())
+        {
+            time = Date.now();
+            global_recognizer = scores[6].word;
+            score = go_score;
+        }
+        else if (go_score < yes_score && yes_score > threshold && time + time_th < Date.now())
+        {
+            time = Date.now();
+            global_recognizer = scores[18].word;
+            score = yes_score;
+        }
+        */
+
+        scores.sort((s1, s2) => s2.score - s1.score);
+        console.log("SPEECH RECOGNIZED :: " + scores[0].word);
+    }, {probabilityThreshold: threshold});
 }
 
 async function app() {
- recognizer = speechCommands.create('BROWSER_FFT');
- await recognizer.ensureModelLoaded();
- predictWord();
+    recognizer = speechCommands.create('BROWSER_FFT');
+    await recognizer.ensureModelLoaded();
+    predictWord();
 }
 
 app();
